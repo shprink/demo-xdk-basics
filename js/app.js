0b26103c3dcbfd97fd070e0eb3f7792c6aea11bf
shprink <julien.renaux@gmail.com>
2014-02-02 22:07:02 +0100

    working on menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var app = angular.module('demobasics', ['ngRoute', 'ngProgress', 'menuSlider']).config(function($routeProvider, $sceDelegateProvider) {
+var app = angular.module('demobasics', ['ngRoute', 'ngProgress', 'snap']).config(function($routeProvider, $sceDelegateProvider, snapRemoteProvider) {
+    snapRemoteProvider.globalOptions.disable = 'right';
 	$routeProvider.when('/list', {
 		templateUrl: 'views/list.html',
 		controller: ListCtrl
@@ -10,7 +11,7 @@ var app = angular.module('demobasics', ['ngRoute', 'ngProgress', 'menuSlider']).
 	});
 });
 
-function ListCtrl($scope, $routeParams, ngProgress) {
+function ListCtrl($scope, $routeParams, ngProgress, snapRemote) {
         ngProgress.start();
         ngProgress.height('5px');
         $scope.loaded = false;
@@ -22,9 +23,19 @@ function ListCtrl($scope, $routeParams, ngProgress) {
                 return '';
         }
         ngProgress.complete();
+//    
+//    snapRemote.getSnapper().then(function(snapper) {
+//      snapper.on('open', function() {
+//        logger.info('Opened!');
+//      });
+//
+//      snapper.on('close', function() {
+//        logger.info('Closed!');
+//      });
+//    });
 }
 
-ListCtrl.$inject = ['$scope','$routeParams', 'ngProgress'];
+ListCtrl.$inject = ['$scope','$routeParams', 'ngProgress', 'snapRemote'];
 
 function NotificationsCtrl($scope, $routeParams, ngProgress) {
         ngProgress.start();
