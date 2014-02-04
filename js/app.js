e437e0375efbf81b827b0fca9c08c75871fec43b
shprink <julien.renaux@gmail.com>
2014-02-05 00:57:56 +0100

    more

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('demobasics', ['ngRoute', 'ngProgress', 'ngTouch', 'snap'])
+var app = angular.module('demobasics', ['ngRoute', 'ngProgress', 'ngTouch', 'snap', 'ajaxFactory'])
     .config(function($routeProvider, snapRemoteProvider) { // provider-injector
         snapRemoteProvider.globalOptions.disable = 'right';
         snapRemoteProvider.globalOptions.hyperextensible = false;
@@ -6,29 +6,37 @@ var app = angular.module('demobasics', ['ngRoute', 'ngProgress', 'ngTouch', 'sna
         $routeProvider.when('/list', {
             templateUrl: 'views/list.html',
             controller: ListCtrl
+        }).when('/home', {
+            templateUrl: 'views/home.html',
+            controller: HomeCtrl
         }).when('/notifications', {
             templateUrl: 'views/notifications.html',
             controller: NotificationsCtrl
+        }).when('/item/:id', {
+            templateUrl: 'views/item.html',
+            controller: NotificationsCtrl
         }).otherwise({
-            redirectTo: '/list'
+            redirectTo: '/home'
         });
 });
 
-app.run(['$rootScope', 'demoService', 'snapRemote', function($rootScope, demoService, snapRemote){
-    $rootScope.demoService = demoService;
+app.run(['$rootScope', 'snapRemote', 'ngProgress', 'Ajax', '$log', function($rootScope, snapRemote, ngProgress, Ajax, $log){
+    ngProgress.height('5px');
+     $rootScope.menuClose = function(){
+        snapRemote.close();
+     }
+     $rootScope.menuStart = function(){
+        snapRemote.start();
+     }
+     
+     $rootScope.menus = Ajax.query({
+            file: 'menu'
+            }, function(demo) {
+                $rootScope.$broadcast( 'menu.success' );
+            }, function(error) {
+                $rootScope.$broadcast( 'menu.error' );
+        });
 }]);
-
-//app.provider("$test", function(){
-//    this.$get = [function(){
-//       var menu = {};
-//       menu.toggle = function toggle() {
-//           var body = angular.element(document.querySelector('body'));
-//           body.toggleClass('menu-shown');              
-//       };
-//
-//       return menu;
-//    }];
-//});
 
 app.service('demoService', function() {
 	return new (function() {
@@ -42,13 +50,45 @@ app.service('demoService', function() {
     })();
 });
 
+//module.service( 'Menu', [ '$rootScope', 'Ajax', function( $rootScope, Ajax ) {
+//    var service = {
+//        books: [
+//            { title: "Magician", author: "Raymond E. Feist" },
+//            { title: "The Hobbit", author: "J.R.R Tolkien" }
+//        ],
+//        
+//        addBook: function ( book ) {
+//            service.books.push( book );
+//            
+//        }
+//        
+//        Ajax.get({
+//            file: 'menu'
+//            }, function(demo) {
+//                
+//                $rootScope.$broadcast( 'menu.success' );
+//            }, function(error) {
+//                $rootScope.$broadcast( 'menu.error' );
+//        });
+//    }
+//    
+//    return service;
+//}]);
+
+angular.module('ajaxFactory', ['ngResource'])
+		.factory('Ajax', function($resource) {
+	return $resource('data/:file.json', {}, {
+		query: {
+            method:'GET',
+			isArray: true,
+			cache: false
+		}
+	});
+});
+
 function ListCtrl($scope, $routeParams, ngProgress, snapRemote) {
-    $scope.close = function(){
-         snapRemote.close();
-    }
-   
+    snapRemote.close();
     ngProgress.start();
-    ngProgress.height('5px');
     $scope.loaded = false;
 
     $scope.getRoute = function() {
@@ -65,8 +105,23 @@ ListCtrl.$inject = ['$scope','$routeParams', 'ngProgress', 'snapRemote'];
 function NotificationsCtrl($scope, $routeParams, ngProgress, snapRemote) {
         snapRemote.close();
         ngProgress.start();
-        ngProgress.height('5px');
         ngProgress.complete();
 }
 
 NotificationsCtrl.$inject = ['$scope','$routeParams', 'ngProgress', 'snapRemote'];
+
+function HomeCtrl($scope, $routeParams, ngProgress, snapRemote) {
+        snapRemote.close();
+        ngProgress.start();
+        ngProgress.complete();
+}
+
+HomeCtrl.$inject = ['$scope','$routeParams', 'ngProgress', 'snapRemote'];
+
+function ItemCtrl($scope, $routeParams, ngProgress, snapRemote) {
+        snapRemote.close();
+        ngProgress.start();
+        ngProgress.complete();
+}
+
+ItemCtrl.$inject = ['$scope','$routeParams', 'ngProgress', 'snapRemote'];
